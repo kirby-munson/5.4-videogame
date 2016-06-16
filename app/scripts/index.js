97fd0c730a395793b04c06014e23407ca2b457a0
Kirby Munson <kmunson@g.clemson.edu>
2016-06-16 14:01:43 -0400

    added some basic conent

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,41 @@
 console.log("Hello World!");
+
+var $ = require('jquery');
+var _ = require('underscore');
+var models = require('./models');
+var template = require('../template/puppy-template.hbs');
+
+$(function(){
+
+  var favoritePuppy
+
+var puppies = [
+  new models.GoodGuy({name: 'Spiderman'});
+  new models.Goodguy({name: 'Batman'});
+];
+
+_.each(puppies, function(puppy){
+  var html = template(puppy);
+  $('js-puppies').append(html);
+
+
+
+$('js-cute-puppy').on('click', function(event){
+  event.preventDefault();
+
+  var $puppyImage = $(this);
+  var puppyName = $puppyImage.data('name');
+  favoritePuppy_.filter(puppies, {'name': puppyName});
+
+  alert('My name is' + favoritePuppy.name + 'and i have a' favoritePuppy.speak());
+
+}
+
+
+
+
+
+
+
+
+});
---
+++ app/scripts/models.js
@@ -0,0 +1,38 @@
+var $ = require('jquery');
+
+
+function Character(config){
+  config = config || {};
+  $.extend(this, config);
+  this.isCute = true;
+
+}
+
+Character.prototype.bigEyes = true;
+
+
+function GoodGuy(config){
+  CuteAnimals.call(this, config);
+}
+GoodGuy.prototype = new Character();
+Goodguy.prototype.speak = function(){
+  return 'tiny bark';
+}
+
+
+function BadGuy(config){
+  CuteAnimals.call(this, config);
+}
+
+BadGuy.prototype = new Character();
+BadGuy.prototype.speak = function(){
+  return 'purrrrrrrrrr';
+}
+
+
+
+
+module.export = {
+  'GoodGuy': GoodGuy,
+  'BadGuy' : BadGuy
+}
