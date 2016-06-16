687c187289768fea440272e779c7011fdb1abe1e
vierello <vierello@hotmail.com>
2016-06-16 17:17:41 -0400

    character select working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,52 +1,49 @@
-console.log("Hello World!");
-
 var $ = require('jquery');
 var _ = require('underscore');
 var models = require('./models');
 var goodTemplate = require('../templates/good-template.hbs');
-var badTemplate = require('../templates/bad-template.hbs')
+var badTemplate = require('../templates/bad-template.hbs');
 
 $(function(){
 
+  var goodGuys = [
+    new models.GoodGuy({name: 'Dan', image: 'http://unsplash.it/100/100'}),
+    new models.GoodGuy({name: 'Faith', image: 'http://unsplash.it/100/100'})
+  ];
 
 
-var goodGuys = [
-  new models.GoodGuy({name: 'Dan', image: 'http://unsplash.it/100/100'});
-  new models.Goodguy({name: 'Batman'});
+  var badGuys = [
+    new models.BadGuy({name: 'Joel', image: 'http://unsplash.it/110/110'})
+  ];
 
-];
+goodGuys.forEach(function(character, index, array){
+  $('#characters').append('<option value="' + index + '">' + character.name + '</option>');
+});
+// _.each(goodGuys, function(goodGuy){
+//   var html = goodTemplate(goodGuy);
+//   $('js-puppies').append(html);
+//}
 
-_.each(goodGuys, function(goodGuy){
-  var html = goodTemplate(goodGuy);
-  $('js-puppies').append(html);
 
+// $('js-cute-puppy').on('click', function(event){
+//   event.preventDefault();
+//
+//   var $puppyImage = $(this);
+//   var puppyName = $puppyImage.data('name');
+//   favoritePuppy_.filter(puppies, {'name': puppyName});
+//
+//   alert('My name is' + favoritePuppy.name + 'and i have a' favoritePuppy.speak());
+//
+// }
+  var goodContext = {'goodGuys': goodGuys};
+  var badContext = {'badGuys': badGuys};
+  var $charSelect = $('#characters');
 
-
-$('js-cute-puppy').on('click', function(event){
-  event.preventDefault();
-
-  var $puppyImage = $(this);
-  var puppyName = $puppyImage.data('name');
-  favoritePuppy_.filter(puppies, {'name': puppyName});
-
-  alert('My name is' + favoritePuppy.name + 'and i have a' favoritePuppy.speak());
-
-}
-
-  var $charSelect = $('select option')
-
-  $charSelect.click(function(){
+  $charSelect.change(function(){
     event.preventDefault();
     var $clickedChar = $(this);
-
-    $('.good-template').html(goodTemplate);
-    $('.bad-template').html(badTemplate);
-    $('.active').removeClass('active');
-    $clickedLink.addClass('active');
-
-
-
-
-
-
+    console.log(this.name);
+    $('.good-template').html(goodTemplate(goodContext));
+    $('.bad-template').html(badTemplate(badContext));
+  });
 });
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,56 +1,48 @@
 var $ = require('jquery');
 
-
-function Character(characterTraits){
+function Character(config){
   config = config || {};
   $.extend(this, config);
-
-}
+};
 Character.prototype.attack = function(){
   return name + ' attacks with ' + weapon;
-}
+};
 
 
 function GoodGuy(config){
   Character.call(this, config);
-
-  console.log();
-}
+};
 GoodGuy.prototype = new Character();
-Goodguy.prototype.speak = function(){
-  return 'tiny bark';
-}
 
-var Dan = new GoodGuy()
-var Kara = new GoodGuy()
-var Kurt = new GoodGuy()
-var Rob = new GoodGuy()
-var Andy = new GoodGuy()
-var Kirby = new GoodGuy()
-var Christian = new GoodGuy()
-var Matthew = new GoodGuy()
-var David = new GoodGuy()
+
+// var Dan = new GoodGuy()
+// var Kara = new GoodGuy()
+// var Kurt = new GoodGuy()
+// var Rob = new GoodGuy()
+// var Andy = new GoodGuy()
+// var Kirby = new GoodGuy()
+// var Christian = new GoodGuy()
+// var Matthew = new GoodGuy()
+// var David = new GoodGuy()
 
 function BadGuy(config){
   Character.call(this, config);
-}
+};
 
 BadGuy.prototype = new Character();
-BadGuy.prototype.speak = function(){
-  return 'purrrrrrrrrr';
-}
 
-var Joel = new BadGuy()
-var Hope = new BadGuy()
-var Rayn = new BadGuy()
-var Emily = new BadGuy()
-var Bill = new BadGuy()
-var Chuck= new BadGuy()
-var Eileen = new BadGuy()
-var Davis = new BadGuy()
-var John = new BadGuy()
 
-module.export = {
+// var Joel = new BadGuy()
+// var Hope = new BadGuy()
+// var Rayn = new BadGuy()
+// var Emily = new BadGuy()
+// var Bill = new BadGuy()
+// var Chuck= new BadGuy()
+// var Eileen = new BadGuy()
+// var Davis = new BadGuy()
+// var John = new BadGuy()
+
+module.exports = {
   'GoodGuy': GoodGuy,
   'BadGuy' : BadGuy
-}
+};
