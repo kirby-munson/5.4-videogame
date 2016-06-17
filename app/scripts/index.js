22c7616a00e4593e2e6143fff1b989b373099041
vierello <vierello@hotmail.com>
2016-06-17 14:57:34 -0400

    A: js and html changes

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,15 +7,15 @@ var badTemplate = require('../templates/bad-template.hbs');
 $(function(){
 
   var goodGuys = [
-    new models.GoodGuy({name: 'Dan', image: '../app/images/frontend-dan.png'}),
-    new models.GoodGuy({name: 'Rob', image: '../app/images/frontend-rob.png'}),
-    new models.GoodGuy({name: 'Kara', image: '../app/images/frontend-kara.png'}),
-    new models.GoodGuy({name: 'Kurt', image: '../app/images/frontend-kurt.png'}),
-    new models.GoodGuy({name: 'Andy', image: '../app/images/frontend-andy.png'}),
-    new models.GoodGuy({name: 'Kirby', image: '../app/images/frontend-kirby.png'}),
-    new models.GoodGuy({name: 'Christian', image: '../app/images/frontend-christian.png'}),
-    new models.GoodGuy({name: 'Matthew', image: '../app/images/frontend-matthew.png'}),
-    new models.GoodGuy({name: 'David', image: '../app/images/frontend-david.png'})
+    new models.GoodGuy({name: 'Dan', weapon: 'all the codes', image: '../app/images/frontend-dan.png'}),
+    new models.GoodGuy({name: 'Kara', weapon: 'scope', image: '../app/images/frontend-kara.png'}),
+    new models.GoodGuy({name: 'Kurt',  weapon: 'iterators', image: '../app/images/frontend-kurt.png'}),
+    new models.GoodGuy({name: 'Rob', weapon: 'functions', image: '../app/images/frontend-rob.png'}),
+    new models.GoodGuy({name: 'Andy', weapon: 'variables', image: '../app/images/frontend-andy.png'}),
+    new models.GoodGuy({name: 'Kirby', weapon: 'constructors', image: '../app/images/frontend-kirby.png'}),
+    new models.GoodGuy({name: 'Christian', weapon: 'syntax', image: '../app/images/frontend-christian.png'}),
+    new models.GoodGuy({name: 'Matthew', weapon: 'html', image: '../app/images/frontend-matt.png'}),
+    new models.GoodGuy({name: 'David', weapon:'css', image: '../app/images/frontend-david.png'})
   ];
 
   //console.log(goodGuys);
@@ -31,37 +31,51 @@ $(function(){
     new models.BadGuy({name: 'Chuck', weapon: 'python', image: '../app/images/backend-emily.png'}),
   ];
 
+
+console.log(enemyNumber);
 goodGuys.forEach(function(character, index, array){
   $('#characters').append('<option value="' + index + '">' + character.name + '</option>');
 });
 
-  var goodContext = {'goodGuys': goodGuys};
-  var badContext = {'badGuys': badGuys};
+
+  // var goodContext = {'goodGuys': goodGuys};
+  // var badContext = {'badGuys': badGuys};
+  var enemyNumber;
   var $charSelect = $('#characters');
   //console.log(goodContext);
   $charSelect.change(function(event){
     event.preventDefault();
+    enemyNumber = (Math.floor(Math.random()*9));
     var $clickedChar = this.options[this.selectedIndex];
-    console.log($clickedChar);
-    //console.log($clickedChar.getAttribute('value'));
+    // console.log($clickedChar);
+    // console.log($clickedChar.getAttribute('value'));
+
+    var myFighter;
 
     goodGuys.forEach(function(displayedChar, index, array){
       if($clickedChar.getAttribute('value') == index){
-        console.log(displayedChar);
-        $('.good-template').html(goodTemplate({name: displayedChar.name, image: displayedChar.image}));
+        myFighter = displayedChar;
       }
-
-
     });
+    $('.good-template').html(goodTemplate({name: myFighter.name, weapon: myFighter.weapon, image: myFighter.image}));
 
-
-    if(this.selectedIndex === 0){
+    if(this.selectedIndex == ""){
       $("#selectError").html('Please select a Coder to do battle with!');
+      $('.good-template').html(goodTemplate());
     } else {
       $("#selectError").html('');
     }
 
 
+    var myOpponent;
+    badGuys.forEach(function(displayedCharBad, index, array){
+      if(enemyNumber == index){
+        console.log(enemyNumber);
+        console.log(displayedCharBad.name);
+        myOpponent = displayedCharBad;
+      }
+    });
+    $('.bad-template').html(badTemplate({name: myOpponent.name, weapon: myOpponent.weapon, image: myOpponent.image}));
   });
 
 });
